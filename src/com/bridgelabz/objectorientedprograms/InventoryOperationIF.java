8c41502a31967632fd3e10ff6b91aa0a4aec114d
777shetty <shetty.chetu7@gmail.com>
2021-09-23 16:30:44 +0530

    ADDED:Interface Method to be Implimented

---
+++ src/com/bridgelabz/objectorientedprograms/InventoryOperationIF.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.objectorientedprograms;
+
+import com.bridgelabz.objectorientedprograms.Inventory;
+public interface InventoryOperationIF {
+	public Inventory addInventory();
+	public void calculateValue(Inventory[] inventoryArray);
+}
